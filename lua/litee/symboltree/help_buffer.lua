9598bc008eb220900d46f1b5e4a2737f5123e2f7
ldelossa <louis.delos@gmail.com>
2022-09-18 00:55:27 -0400

    fix duplicate help buffer create error

--- lua/litee/symboltree/help_buffer.lua
+++ lua/litee/symboltree/help_buffer.lua
@@ -1,6 +1,9 @@
 local config = require('litee.symboltree.config').config
 
 local M = {}
+
+local help_buf_handle = nil
+
 -- _setup_help_buffer performs an idempotent creation
 -- of the symboltree help buffer
 --
@@ -9,48 +12,46 @@ local M = {}
 --
 -- returns:
 --   "buf_handle"  -- handle to a valid symboltree help buffer
-function M._setup_help_buffer(help_buf_handle)
-    if
-        help_buf_handle == nil
-        or not vim.api.nvim_buf_is_valid(help_buf_handle)
-    then
-        local buf = vim.api.nvim_create_buf(false, false)
-        if buf == 0 then
-            vim.api.nvim_err_writeln("ui.help failed: buffer create failed")
-            return
-        end
-        help_buf_handle = buf
-        local lines = {}
-        if not config.disable_keymaps then
-            lines = {
-                "SYMBOLTREE HELP:",
-                "press '?' to close",
-                "",
-                "KEYMAP:",
-                config.keymaps.expand .. " - expand a symbol",
-                config.keymaps.collapse .. " - collapse a symbol",
-                config.keymaps.collapse_all .. " - collapse all symbols",
-                config.keymaps.jump .. " - jump to a symbol in last used window",
-                config.keymaps.jump_split .. " - jump to symbol in a new split",
-                config.keymaps.jump_vsplit .. " - jump to symbol in a new vertical split",
-                config.keymaps.jump_tab .. " - jump to symbol in a new tab",
-                config.keymaps.hover .. " - show hover info for symbol",
-                config.keymaps.close .. " - close the symboltree component",
-                config.keymaps.details .. " - show symbol details",
-                config.keymaps.close_panel_pop_out .. " - close the popout panel when symboltree is popped out",
-                config.keymaps.help .. " - show help",
-                config.keymaps.hide .. " - hide the symboltree component",
-            }
-        else
-            lines = {
-                "SYMBOLTREE HELP:",
-                "press '?' to close",
-                "",
-                "No KEYMAP set:",
-            }
-        end
-        vim.api.nvim_buf_set_lines(help_buf_handle, 0, #lines, false, lines)
+function M._setup_help_buffer()
+    if help_buf_handle ~= nil and vim.api.nvim_buf_is_valid(help_buf_handle) then
+        return help_buf_handle
     end
+    local buf = vim.api.nvim_create_buf(false, false)
+    if buf == 0 then
+        vim.api.nvim_err_writeln("ui.help failed: buffer create failed")
+        return
+    end
+    help_buf_handle = buf
+    local lines = {}
+    if not config.disable_keymaps then
+        lines = {
+            "SYMBOLTREE HELP:",
+            "press '?' to close",
+            "",
+            "KEYMAP:",
+            config.keymaps.expand .. " - expand a symbol",
+            config.keymaps.collapse .. " - collapse a symbol",
+            config.keymaps.collapse_all .. " - collapse all symbols",
+            config.keymaps.jump .. " - jump to a symbol in last used window",
+            config.keymaps.jump_split .. " - jump to symbol in a new split",
+            config.keymaps.jump_vsplit .. " - jump to symbol in a new vertical split",
+            config.keymaps.jump_tab .. " - jump to symbol in a new tab",
+            config.keymaps.hover .. " - show hover info for symbol",
+            config.keymaps.close .. " - close the symboltree component",
+            config.keymaps.details .. " - show symbol details",
+            config.keymaps.close_panel_pop_out .. " - close the popout panel when symboltree is popped out",
+            config.keymaps.help .. " - show help",
+            config.keymaps.hide .. " - hide the symboltree component",
+        }
+    else
+        lines = {
+            "SYMBOLTREE HELP:",
+            "press '?' to close",
+            "",
+            "No KEYMAP set:",
+        }
+    end
+    vim.api.nvim_buf_set_lines(help_buf_handle, 0, #lines, false, lines)
     -- set buf options
     vim.api.nvim_buf_set_name(help_buf_handle, "Symboltree Help")
     vim.api.nvim_buf_set_option(help_buf_handle, 'bufhidden', 'hide')
